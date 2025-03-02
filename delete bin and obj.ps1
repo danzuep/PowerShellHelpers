d2eb609d22fbd8f5b55323097dc312329a19237a
Daniel Collingwood <82693586+danzuep@users.noreply.github.com>
2025-03-02 17:58:52 +0800

    Delete bin and obj folders in a more performant way

--- delete bin and obj.ps1
+++ delete bin and obj.ps1
@@ -1,6 +1,13 @@
-$foldersToDelete = @('*/bin/', '*/obj/', '*/.vs', './*/packages/')
-Write-Output "Deleting folders: " | %{$_ + $foldersToDelete}
-Remove-Item $foldersToDelete -Recurse -Force -ErrorAction SilentlyContinue
+$foldersToDelete = @('bin', 'obj', '.vs', 'packages')
+$foldersToDelete | ForEach-Object {
+    Write-Output "Searching for '$_' folders..."
+    Get-ChildItem -Path '.' -Recurse -Directory -Filter $_ -Force | ForEach-Object {
+        Write-Output "Deleting folder: $($_.FullName)"
+        Remove-Item -Path $_.FullName -Recurse -Force -ErrorAction SilentlyContinue
+    }
+}
 pause
 
-# Get-ChildItem -Path '.' -Recurse -Directory -Include bin, obj | Remove-Item -Recurse -Force
+# @('bin','obj') | %{ gci -r -di -fi $_ | rm -r -fo }
+# @('bin','obj') | ForEach-Object { Remove-Item $_ -Recurse -Force -ErrorAction SilentlyContinue }
+# @('bin','obj') | ForEach-Object { Get-ChildItem -Recurse -Directory -Filter $_ -Force | ForEach-Object { Write-Host "Deleting folder: $($_.FullName)"; Remove-Item $($_.FullName) -Recurse -Force -ErrorAction SilentlyContinue } }; dotnet nuget locals all --clear
